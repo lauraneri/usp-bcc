58e491c3ed59ca92e270e37c8b73c58a76e856e1
Laura Neri <78615562+lauraneri@users.noreply.github.com>
2024-03-18 09:54:32 -0300

    Add files via upload

---
+++ grafos/matriz_de_adjacencias/graph.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "graph.h"
+
+#define vertex int
+
+struct graph_{
+    int V;
+    int A;
+    vertex **matrix;
+};
+
+vertex** create_matrix(int n){
+    vertex **m = (vertex **) malloc(n * sizeof(vertex*));
+
+    if(m == NULL){
+    //printf("Erro de alocação de memória.");
+    return NULL;  
+    }
+
+    for (int i=0; i<n; i++){
+        m[i] = (vertex*) malloc (n * sizeof(vertex));
+
+        if(m[i] == NULL){
+        //printf("Erro de alocação de memória.");
+        return NULL;  
+    }
+    }
+    for (int i=0; i<n; i++){
+        for (int j=0; j<n; j++){
+            m[i][j] = 0;
+        }
+    }
+
+    return m;
+}
+
+GRAPH *create_graph(int v){
+    GRAPH *g = (GRAPH *) malloc(sizeof(GRAPH));
+
+    if(g == NULL){
+    //printf("Erro de alocação de memória.");
+    return NULL;  
+    }
+
+    g->V = v;
+    g->A = 0;
+    g->matrix = create_matrix(v);
+
+    return g;
+}
