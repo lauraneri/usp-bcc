ca82e256e007a0f93be6288a9d311e022f3d02e3
Laura Neri <laura6neri@yahoo.com.br>
2024-03-25 16:14:31 -0300

    uploading new directory where I'll code more in the future

---
+++ organizacao-e-arquitetura-de-computadores/codigos/riscv1.asm
@@ -0,0 +1,36 @@
+	.data
+	.align 0
+str1: 	.asciz "Hello World++!"
+str2:	.asciz "Hello World--!"
+	
+	.text
+	.align 2
+	.globl main
+
+main:
+	addi a7, zero, 5
+	ecall
+	
+	add s0, a0, zero
+	blt s0, zero, print_neg
+	
+	addi a7, zero, 4
+	la a0, str1
+	ecall
+	
+	j the_end
+	
+print_neg:
+	addi a7, zero, 4
+	la a0, str2
+	ecall
+
+the_end:
+	addi a7, zero, 1
+	add a0, zero, s0
+	ecall
+	
+	li a7, 10
+	ecall
+	
+	
