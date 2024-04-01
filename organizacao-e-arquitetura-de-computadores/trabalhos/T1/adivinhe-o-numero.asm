1ace81097c5f316d74365db8a9a15bc4641092ea
Laura Neri <laura6neri@yahoo.com.br>
2024-04-01 12:15:45 -0300

    update: program asks user an integer number and prints it

--- organizacao-e-arquitetura-de-computadores/trabalhos/T1/adivinhe-o-numero.asm
+++ organizacao-e-arquitetura-de-computadores/trabalhos/T1/adivinhe-o-numero.asm
@@ -1,25 +1,38 @@
 	.data
 	.align 0	#alinhando para string
-initMsg:.asciz "Bem-vindo! Este é o jogo 'Adivinhe o número'!\n"
-instMsg:.asciz "O computador escolheu um número entre 1 e 100, você consegue adivinhar qual é? Faça seu chute!\n"
+initMsg:.asciz "Bem-vindo! Este é o jogo 'Adivinhe o número'!\nO computador escolheu um número entre 1 e 100, você consegue adivinhar qual é? Faça seu chute!"
 			#mensagens de boas-vindas e instruções
+chuteMsg: .asciz "Chute: "
+finalMsg: .asciz "\nFim! Obrigada por jogar! :D\n"
 	
 	.align 2	#alinhando para inteiro
-numero: .word
-	
+
+
 	.text
 	.align 2
 	.globl main
 	
 main:
-	addi a7, zero, 4
+	addi a7, zero, 4 #printando msg de boas vindas e instruções
 	la a0, initMsg
-	ecall		#printando msg de boas vindas
+	ecall			
 	
-	addi a7, zero, 4
-	la a0, instMsg
-	ecall		#printando msg de instruções
+	addi a7, zero, 5 #solicitando entrada de inteiro (chute de numero)
+	ecall		
 	
+	add s0, a0, zero #armazenando o chute
+	
+	addi a7, zero, 4 #printando o chute
+	la a0, chuteMsg
+	ecall
+	
+	addi a7, zero, 1
+	add a0, zero, s0
+	ecall		#--
+	
+	addi a7, zero, 4 #printando msg final
+	la a0, finalMsg
+	ecall
 	
 	
 	addi a7, zero, 10 #finalizando programa
