6f37ac57fabc559cac2a6534949291ef4827765c
Laura Neri <laura6neri@yahoo.com.br>
2024-04-01 16:33:51 -0300

    testing lcg algorithm

---
+++ organizacao-e-arquitetura-de-computadores/codigos/teste-lcg.py
@@ -0,0 +1,23 @@
+""" Teste para compreensão de gerador congruencial linear (LCG em inglês)"""
+
+def lcg(modulo, a, c, semente=None):
+    if semente != None:
+        lcg.anterior = semente
+    num_aleatorio = (lcg.anterior * a + c) % modulo
+    lcg.anterior = num_aleatorio
+    return num_aleatorio
+lcg.anterior = 2222
+
+lista = []
+existe = False
+for i in range (102):
+    num = lcg(100, 21, 17)
+    if num in lista:
+        existe = True
+        print("ACHOU")
+    lista.append(num)
+    print(num)
+
+if existe == False:
+    print("NAO ACHOU")
+    
